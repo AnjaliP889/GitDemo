ef3b46e873efced1373547b84b5122a8db8b14c0
anjali <singhanjali889@gmail.com>
2022-07-25 21:42:37 +0530

    Feature 6 by Lead Dev

--- src/test/java/qa/Mavenjava/RestAPITest.java
+++ src/test/java/qa/Mavenjava/RestAPITest.java
@@ -11,6 +11,7 @@ public class RestAPITest {
 			System.out.println("PostJira2");
 			System.out.println("PostJira3");
 			System.out.println("PostJira4");
+			System.out.println("Feature 6 developed by Lead dev");
 		}
 		@Test
 		public void deleteTwitter()
