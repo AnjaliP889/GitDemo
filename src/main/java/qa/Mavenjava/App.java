962007fd5f49bd839615843dbbdc0f220699be61
anjali <singhanjali889@gmail.com>
2022-07-25 19:53:50 +0530

    First commit

---
+++ src/main/java/qa/Mavenjava/App.java
@@ -0,0 +1,13 @@
+package qa.Mavenjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/qa/Mavenjava/AppiumTest.java
@@ -0,0 +1,18 @@
+package qa.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+@Test
+public void nativeAppAndroid()
+{
+	System.out.println("NativeAppAndroid");
+}
+@Test
+public void IOSApp()
+{
+	System.out.println("IOSApp");
+}
+
+}
