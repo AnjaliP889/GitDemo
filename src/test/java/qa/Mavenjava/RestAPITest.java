1088873cbf631f277986696f3623f2470d2f9a9d
anjali <singhanjali889@gmail.com>
2022-07-25 21:26:48 +0530

    This is from Dev2

--- src/test/java/qa/Mavenjava/RestAPITest.java
+++ src/test/java/qa/Mavenjava/RestAPITest.java
@@ -10,6 +10,7 @@ public class RestAPITest {
 			System.out.println("PostJira");
 			System.out.println("PostJira2");
 			System.out.println("PostJira3");
+			System.out.println("PostJira4");
 		}
 		@Test
 		public void deleteTwitter()
